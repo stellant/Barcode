53ce13aa691ced01103f639cfa3930861bbbc7fc
stellant <thangavel.ayyasamy@wipro.com>
2015-09-09 23:12:52 +0530

    Fourth Commit

---
+++ LiveView/SrLiveViewSample/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace SrLiveViewSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.barcodeReaderControl1 = new Keyence.AutoID.BarcodeReaderControl();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButtonImageErrSave = new System.Windows.Forms.RadioButton();
+            this.radioButtonImageOkSave = new System.Windows.Forms.RadioButton();
+            this.radioButtonImageNoSave = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // barcodeReaderControl1
+            // 
+            this.barcodeReaderControl1.BackColor = System.Drawing.Color.Lime;
+            this.barcodeReaderControl1.IpAddress = "0.0.0.0";
+            this.barcodeReaderControl1.LiveView.WindowScale = 2;
+            this.barcodeReaderControl1.Location = new System.Drawing.Point(14, 17);
+            this.barcodeReaderControl1.Name = "barcodeReaderControl1";
+            this.barcodeReaderControl1.ReaderType = Keyence.AutoID.ReaderType.SR_D100;
+            this.barcodeReaderControl1.Size = new System.Drawing.Size(400, 325);
+            this.barcodeReaderControl1.TabIndex = 44;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Checked = true;
+            this.radioButton3.Location = new System.Drawing.Point(27, 83);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(67, 18);
+            this.radioButton3.TabIndex = 43;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "SR-1000";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(27, 38);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(61, 18);
+            this.radioButton2.TabIndex = 42;
+            this.radioButton2.Text = "SR-750";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(27, 17);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(69, 18);
+            this.radioButton1.TabIndex = 41;
+            this.radioButton1.Text = "SR-D100";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(210, 352);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(190, 53);
+            this.button6.TabIndex = 40;
+            this.button6.Text = "Start LiveView";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(430, 77);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(243, 125);
+            this.textBox3.TabIndex = 38;
+            this.textBox3.Text = "IP address = 192.168.100.100\r\n\r\nCommand Listenning port = 9003\r\n\r\nData Listenning" +
+                " Port  = 9004";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(428, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "Reader Configuration";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 588);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 36;
+            this.label2.Text = "Read Data";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox2.Location = new System.Drawing.Point(14, 612);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(587, 121);
+            this.textBox2.TabIndex = 35;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 488);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Status";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox1.Location = new System.Drawing.Point(14, 510);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(587, 51);
+            this.textBox1.TabIndex = 33;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(14, 352);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(190, 53);
+            this.button3.TabIndex = 32;
+            this.button3.Text = "Connect";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 424);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 53);
+            this.button2.TabIndex = 31;
+            this.button2.Text = "LOFF";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 424);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 53);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "LON";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton6);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(430, 209);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(120, 112);
+            this.groupBox1.TabIndex = 45;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Reader Type";
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(27, 61);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(61, 18);
+            this.radioButton6.TabIndex = 44;
+            this.radioButton6.Text = "SR-700";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Location = new System.Drawing.Point(556, 209);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(120, 112);
+            this.groupBox2.TabIndex = 46;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Interface";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Checked = true;
+            this.radioButton4.Location = new System.Drawing.Point(20, 77);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(84, 18);
+            this.radioButton4.TabIndex = 43;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "ETHERNET";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(20, 53);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(47, 18);
+            this.radioButton5.TabIndex = 42;
+            this.radioButton5.Text = "USB";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButtonImageErrSave);
+            this.groupBox3.Controls.Add(this.radioButtonImageOkSave);
+            this.groupBox3.Controls.Add(this.radioButtonImageNoSave);
+            this.groupBox3.Location = new System.Drawing.Point(430, 343);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(182, 104);
+            this.groupBox3.TabIndex = 48;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Saving Images";
+            // 
+            // radioButtonImageErrSave
+            // 
+            this.radioButtonImageErrSave.AutoSize = true;
+            this.radioButtonImageErrSave.Location = new System.Drawing.Point(7, 70);
+            this.radioButtonImageErrSave.Name = "radioButtonImageErrSave";
+            this.radioButtonImageErrSave.Size = new System.Drawing.Size(79, 18);
+            this.radioButtonImageErrSave.TabIndex = 2;
+            this.radioButtonImageErrSave.Text = "Error Image";
+            this.radioButtonImageErrSave.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonImageOkSave
+            // 
+            this.radioButtonImageOkSave.AutoSize = true;
+            this.radioButtonImageOkSave.Location = new System.Drawing.Point(7, 46);
+            this.radioButtonImageOkSave.Name = "radioButtonImageOkSave";
+            this.radioButtonImageOkSave.Size = new System.Drawing.Size(72, 18);
+            this.radioButtonImageOkSave.TabIndex = 1;
+            this.radioButtonImageOkSave.Text = "OK Image";
+            this.radioButtonImageOkSave.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonImageNoSave
+            // 
+            this.radioButtonImageNoSave.AutoSize = true;
+            this.radioButtonImageNoSave.Checked = true;
+            this.radioButtonImageNoSave.Location = new System.Drawing.Point(7, 21);
+            this.radioButtonImageNoSave.Name = "radioButtonImageNoSave";
+            this.radioButtonImageNoSave.Size = new System.Drawing.Size(60, 18);
+            this.radioButtonImageNoSave.TabIndex = 0;
+            this.radioButtonImageNoSave.TabStop = true;
+            this.radioButtonImageNoSave.Text = "Disable";
+            this.radioButtonImageNoSave.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 744);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.barcodeReaderControl1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Keyence.AutoID.BarcodeReaderControl barcodeReaderControl1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButtonImageErrSave;
+        private System.Windows.Forms.RadioButton radioButtonImageOkSave;
+        private System.Windows.Forms.RadioButton radioButtonImageNoSave;
+    }
+}
+
---
+++ LiveView/SrLiveViewSample/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Keyence.AutoID;
+
+namespace SrLiveViewSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //
+            // Register handler for reading data received event
+            //
+            this.barcodeReaderControl1.OnDataReceived += barcodeReaderControl1_OnDataReceived;
+            //
+            // Set the type of reader to connect
+            //
+            this.barcodeReaderControl1.ReaderType = ReaderType.SR_1000;
+            //
+            // Set the interface to connect the reader
+            //
+            this.barcodeReaderControl1.Comm.Interface = Interface.Ethernet;
+            //
+            // Set the IP address of the reader to connect
+            //
+            this.barcodeReaderControl1.IpAddress = "192.168.100.100";
+            //
+            // Set the TCP port number of the reader to connect
+            //
+            // Comannd port number must be different from Data port number
+            //
+            this.barcodeReaderControl1.Ether.CommandPort = 9003;
+            this.barcodeReaderControl1.Ether.DataPort = 9004;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //
+                // Connect to the Reader
+                //
+                this.barcodeReaderControl1.Connect();
+                this.textBox1.Text = "Connected successfully";
+                if (this.barcodeReaderControl1.Comm.Interface == Interface.USB)
+                {
+                    //
+                    // Send "SKCLOSE" in order to occupy the data port connection
+                    //
+                    this.barcodeReaderControl1.SKCLOSE();
+                }
+                else
+                {
+                    //
+                    // Make sure that command response character string is specified.
+                    //
+                    string val = this.barcodeReaderControl1.RP("610");
+                    switch (this.barcodeReaderControl1.ReaderType)
+                    {
+                        case ReaderType.SR_1000:
+                        case ReaderType.SR_700:
+                            if (!val.Equals("1"))
+                            {
+                                this.textBox1.Text = "Set Baseic command response string to Detailed response.";
+                            }
+                            break;
+                        case ReaderType.SR_D100:
+                        case ReaderType.SR_750:
+                            if (!val.Equals("0"))
+                            {
+                                this.textBox1.Text = "Disable the setting of Specify response character.";
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                this.textBox1.Text = ex.Message;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            try
+            {
+                //
+                // Send LON command
+                //
+                this.barcodeReaderControl1.LON();
+            }
+            catch (CommandException cex)
+            {
+                //
+                // ExtErrCode shows the number of command error
+                //
+                this.textBox1.Text = "Command err," + cex.ExtErrCode;
+            }
+            catch (Exception ex)
+            {
+                this.textBox1.Text = ex.Message;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            try
+            {
+                //
+                // Send LOFF command
+                //
+                this.barcodeReaderControl1.LOFF();
+            }
+            catch (Exception ex)
+            {
+                this.textBox1.Text = ex.Message;
+            }
+        }
+
+        private void barcodeReaderControl1_OnDataReceived(object sender, OnDataReceivedEventArgs e)
+        {
+            //
+            // Delegate display processing of the received data to the textBox
+            //
+            this.textBox2.Invoke(new updateTextBoxDelegate(updateTextBox), e.data);
+        }
+
+        //
+        // Delegated function of the textBox
+        //
+        private delegate void updateTextBoxDelegate(byte[] data);
+        private void updateTextBox(byte[] data)
+        {
+            //
+            // Display the received data to the textBox 
+            //
+            this.textBox2.Text = Encoding.GetEncoding("Shift_JIS").GetString(data);
+
+            //
+            // Image file saving process
+            //
+            bool saveImage;
+            if (this.textBox2.Text.StartsWith("ERROR"))
+            {
+                saveImage = radioButtonImageErrSave.Checked;
+
+            }
+            else
+            {
+                saveImage = radioButtonImageOkSave.Checked;
+            }
+            if (saveImage)
+            {
+                try
+                {
+                    //
+                    // Get file path of saved file
+                    //
+                    string srcFile = this.barcodeReaderControl1.LSIMG();
+                    string dstFile = srcFile.Split('\\')[2];
+                    //
+                    // Get image file
+                    //
+                    this.barcodeReaderControl1.GetFile(srcFile, dstFile);
+                    MessageBox.Show(dstFile, "Image file");
+                }
+                catch (Exception ex)
+                {
+                    this.textBox1.Text = ex.Message;
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            try
+            {
+                //
+                // Start processing of LiveView
+                //
+                this.barcodeReaderControl1.StartLiveView();
+            }
+            catch (Exception ex)
+            {
+                this.textBox1.Text = ex.Message;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.radioButton1.Checked)
+            {
+                this.barcodeReaderControl1.StopLiveView();
+                this.barcodeReaderControl1.ReaderType = ReaderType.SR_D100;
+                groupBox3.Enabled = false;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.radioButton2.Checked)
+            {
+                this.barcodeReaderControl1.StopLiveView();
+                this.barcodeReaderControl1.ReaderType = ReaderType.SR_750;
+                groupBox3.Enabled = false;
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.radioButton3.Checked)
+            {
+                this.barcodeReaderControl1.StopLiveView();
+                this.barcodeReaderControl1.ReaderType = ReaderType.SR_1000;
+                groupBox3.Enabled = true;
+            }
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.radioButton5.Checked)
+            {
+                this.barcodeReaderControl1.StopLiveView();
+                this.barcodeReaderControl1.Comm.Interface = Interface.USB;
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.radioButton4.Checked)
+            {
+                this.barcodeReaderControl1.StopLiveView();
+                this.barcodeReaderControl1.Comm.Interface = Interface.Ethernet;
+            }
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.radioButton6.Checked)
+            {
+                this.barcodeReaderControl1.StopLiveView();
+                this.barcodeReaderControl1.ReaderType = ReaderType.SR_700;
+                groupBox3.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
